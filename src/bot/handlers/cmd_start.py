754394ea5268fcd493c3e609bbeeb832bacf5246
Fedor <feda.vert@gmail.com>
2026-03-20 10:41:33 +0300

    Update cmd_start.py

--- src/bot/handlers/cmd_start.py
+++ src/bot/handlers/cmd_start.py
@@ -2,9 +2,12 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram import Router
 
+from src.bot.resources import bot_messages
+
 router = Router()
 
 
 @router.message(CommandStart())
 async def handle_start(message: Message) -> None:
-    await message.answer("✅ Бот запущен и готов работать.")
+    await message.answer(
+bot_messages['/start'])
